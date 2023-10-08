c9394a9c3a4413aa801ee9ba03054429a36568c7
Hamizan Adli Bin Ahmad Shafie <SIHAMIZAN@slake.sssb.com>
2023-10-08 18:30:54 +0800

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+        model.addAttribute("name", principal.getName());
+        model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
+        model.addAttribute("userAttributes", principal.getAttributes());
+        
+        Saml2Authentication auth = (Saml2Authentication) SecurityContextHolder.getContext().getAuthentication();
+        String samlResponse = auth.getSaml2Response();
+        model.addAttribute("samlresponse", samlResponse);
+        return "home";
+    }
+
+}
